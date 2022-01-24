03e61630f82e2424dfb5269de4003d4db3d62632
adefaj <fajimolamide@gmail.com>
2022-01-24 21:21:08 +0100

    commit

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 
-var newList = document.createElement('li');
-var addButton = document.querySelector('.add_todo');
-var todoInput = document.querySelector('#todo_input');
-var todoList = document.querySelector('.todo_list');
+
+const addButton = document.querySelector('.add_todo');
+const editButton = document.querySelector('.edit_todo');
+const delButton = document.querySelector('.del_todo');
+const todoInput = document.querySelector('#todo_input');
+const todoList = document.querySelector('.todo_list');
 
 
+addButton.onclick = function () {
+    var todoText = todoInput.value;
+    if (todoInput === '') {
+        alert('type something!');
+    } else {
+        newList = document.createElement('li');
+        newList.innerHTML = todoText;
+        todoList.insertBefore(newList, todoList.childNodes[0]);
+        todoInput.value = '';
+    }
 
-
-
-
-
-
-
-
-
-
-addButton.addEventListener('click', function () {
-    document.body.append(newList);
-    todoList.append(newList);
-    newList.append(todoInput.value);
     
-});
+}
+
+editButton.onclick = function () {
+
+}
+
+delButton.onclick = function () {
+    var todoText = todoInput.value;
+    todoList.style.display = 'none';
+}
 
