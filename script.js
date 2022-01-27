646116797f3c8e047db54a27954787993579fbb0
adefaj <fajimolamide@gmail.com>
2022-01-27 15:00:16 +0100

    script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 
-
-const addButton = document.querySelector('.add_todo');
+const form = document.querySelector('.main_container');
 const editButton = document.querySelector('.edit_todo');
 const delButton = document.querySelector('.del_todo');
 const todoInput = document.querySelector('#todo_input');
 const todoList = document.querySelector('.todo_list');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    taskList();
+});
 
-addButton.onclick = function () {
-    var todoText = todoInput.value;
-    if (todoInput === '') {
-        alert('type something!');
-    } else {
-        newList = document.createElement('li');
-        newList.innerHTML = todoText;
-        todoList.insertBefore(newList, todoList.childNodes[0]);
-        todoInput.value = '';
-    }
-
-    
+function taskList () {
+    let todoText = todoInput.value;
+    newList = document.createElement('li');
+    newList.innerHTML = todoText;
+    todoList.appendChild(newList);
+    todoInput.value = '';
+    const editButton = document.createElement('div');
+    editButton.innerHTML = `<button class="edit_todo">edit</button>`;
+    newList.appendChild(editButton);
+    const delButton = document.createElement('div');
+    delButton.innerHTML = `<button class="del_todo">delete</button>`;
+    newList.appendChild(delButton);
 }
 
-editButton.onclick = function () {
-
-}
 
 delButton.onclick = function () {
     var todoText = todoInput.value;
